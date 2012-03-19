599032079c436d8f8b1a89b1634c30fc5f6efefa
Roland Arsenault <roland@ccom.unh.edu>
2012-03-19 16:09:59 -0400

    removed debuging send

--- scripts/serial-logger2.py
+++ scripts/serial-logger2.py
@@ -87,7 +87,6 @@ def run(options):
         uaddr = (uaddr,uport)
         udpSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         udpSocket.connect(uaddr)
-        udpSocket.send("hello\n")
     else:
         udpSocket = None
         
